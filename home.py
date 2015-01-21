47d3cf32adf94583ab00ff8e9784b39a578c34ed
Upendra Dhakal <dhakalu@users.noreply.github.com>
2015-01-21 07:58:34 -0600

    Update home.py

--- home.py
+++ home.py
@@ -6,3 +6,7 @@ class Handler(base.Handler):
 
   def get(self):
     self._WriteTemplate('index.html', {})
+  
+  def post(self):
+    subject = self.request.get("subject")
+    tutor = self.request.get("tutor")
