b048c0c6a75c94846e61adb8274fe85a2e904126
Upendra Dhakal <dhakalu@users.noreply.github.com>
2015-01-21 08:57:05 -0600

    Update home.py

--- home.py
+++ home.py
@@ -10,3 +10,15 @@ class Handler(base.Handler):
   def post(self):
     subject = self.request.get("subject")
     tutor = self.request.get("tutor")
+    time = self.request.get("time")
+    location = sle.request.get("location")
+    if not subject or not tutor or not time or not location:
+      output_json['status'] = 'ERR'
+      output_json['error'] = 'All the fields are required fields.'
+    else:
+      tables.TutorSession.create(subject=subject,
+                                 tutor=tutor,
+                                 time=time,
+                                 location=location
+                                 )
+                                 
