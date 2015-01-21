3966f91352267ab5067112e458ecafa379428289
Upendra Dhakal <dhakalu@users.noreply.github.com>
2015-01-21 09:15:18 -0600

    Update tables.py

--- tables.py
+++ tables.py
@@ -1,4 +1,21 @@
 from google.appengine.ext import db
 
 
-class TutorSeesion:
+class TutorSeesion(db.Model):
+  tutor = db.StringProperty(required=True)
+  subject = db.StringProperty(required=True)
+  location = db.StringProperty(required=True)
+  time = db.StringProperty(required=True)
+  
+  @classmethod
+  def create_session(cls, tutor, subject, location, time):
+    return TutorSession(tutor=tutor,
+                        subject=subject,
+                        location=location,
+                        time=time
+                        )
+                        
+    @classmethod
+    def by_id(cls, sessionid):
+      return TutorSession.
+      
